31271863dd6cb7ff7e6d9837784a7df04fc9b3cf
AntSenokosov <a.y.senokosov@gmail.com>
2021-11-27 05:05:17 +0200

    Add class Car

---
+++ Module2HW6/Module2HW6/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2HW6.Models
+{
+    public class Car
+    {
+        public int NumberCar { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public string ModelCar { get; set; }
+        public TypeBody TypeBody { get; set; }
+        public double Price { get; set; }
+        public int Year { get; set; }
+        public double ConsumptionFuel { get; set; }
+    }
+}
