493e33e47116a9f3cb021f9017212b78fac04518
AntSenokosov <a.y.senokosov@gmail.com>
2021-11-28 01:39:28 +0200

    Add class TaxiStationProvider and his interface ITaxtStationProvider

---
+++ Module2HW6/Module2HW6/Providers/TaxiStationProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module2HW6.Models;
+using Module2HW6.Providers.Abstract;
+
+namespace Module2HW6.Providers
+{
+    public class TaxiStationProvider : ITaxiStationProvider
+    {
+        private readonly ITaxiStationProvider _taxiStation;
+        private Car[] _cars;
+
+        public TaxiStationProvider(ITaxiStationProvider taxiStation)
+        {
+            _taxiStation = taxiStation;
+            InitCars();
+        }
+
+        public Car[] GetCars()
+        {
+            return _cars;
+        }
+
+        public void InitCars()
+        {
+        }
+    }
+}
