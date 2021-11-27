545aa37f58b4d20f78de370c2a1a91e8ceb2ae16
AntSenokosov <a.y.senokosov@gmail.com>
2021-11-27 04:46:37 +0200

    Add enum TypeBody

---
+++ Module2HW6/Module2HW6/Models/TypeBody.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2HW6.Models
+{
+    public enum TypeBody
+    {
+		Convertible,
+		Coupe,
+		Hatchback,
+		Sedun,
+		SUV,
+		Van
+	}
+}
