673a3c9f946428cb06d15867f4094d354368d57d
AntSenokosov <a.y.senokosov@gmail.com>
2021-11-28 02:38:58 +0200

    Add class CompareService

---
+++ Module2HW6/Module2HW6/Services/Abstract/ITaxiStationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module2HW6.Models;
+
+namespace Module2HW6.Services.Abstract
+{
+    public interface ITaxiStationService
+    {
+        Car[] GetCars();
+        Car SearchCar();
+        Car[] SortCarByFuel();
+    }
+}
---
+++ Module2HW6/Module2HW6/Services/TaxiStationService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module2HW6.Models;
+using Module2HW6.Providers.Abstract;
+using Module2HW6.Services.Abstract;
+
+namespace Module2HW6.Services
+{
+    public class TaxiStationService : ITaxiStationService
+    {
+        private readonly ITaxiStationProvider _stationProvider;
+        private Car[] _cars;
+        public TaxiStationService(ITaxiStationProvider stationProvider)
+        {
+            _stationProvider = stationProvider;
+            _cars = _stationProvider.GetCars();
+        }
+
+        public Car[] GetCars()
+        {
+            return _cars;
+        }
+
+        public Car SearchCar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car[] SortCarByFuel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
