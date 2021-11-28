47533939fff8b0d409019df375b3fce741e6834c
AntSenokosov <a.y.senokosov@gmail.com>
2021-11-28 19:41:59 +0200

    Add class Sedan

--- Module2HW6/Module2HW6/Models/Car.cs
+++ Module2HW6/Module2HW6/Models/Car.cs
@@ -12,6 +12,7 @@ namespace Module2HW6.Models
         public Manufacturer Manufacturer { get; set; }
         public string ModelCar { get; set; }
         public TypeBody TypeBody { get; set; }
+        public CategoryCar CategoryCar { get; set; }
         public double Price { get; set; }
         public int Year { get; set; }
         public double ConsumptionFuel { get; set; }
